3ad37dc7a8804304b1e68c9b422d50bddcf79f5f
Samuel Cornet <corsam28@gmail.com>
2021-07-25 15:02:05 -0400

    chore: create the first route

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import config from 'config';
 import log from './logger';
 import connect from './db/connect';
+import routes from './routes';
 
 const port = config.get('port') as number;
 const host = config.get('host') as string;
@@ -12,7 +13,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.listen(port, host, () => {
-  log.info(`Server listening at http://${host}:${port}`);
-
+  log.info(`Server listing at http://${host}:${port}`);
   connect();
+  routes(app);
 });
