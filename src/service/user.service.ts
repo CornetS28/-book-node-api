8923e74e3df3ee8453b07264e107dd9aa147e5c6
Samuel Cornet <corsam28@gmail.com>
2021-07-25 16:58:01 -0400

    chore: create sessions service

--- src/service/user.service.ts
+++ src/service/user.service.ts
@@ -2,11 +2,32 @@ import { DocumentDefinition, FilterQuery } from 'mongoose';
 import { omit } from 'lodash';
 import User, { UserDocument } from '../model/user.model';
 
-// create a user
 export async function createUser(input: DocumentDefinition<UserDocument>) {
   try {
     return await User.create(input);
   } catch (error) {
     throw new Error(error);
   }
+}
+
+// create a user
+export async function validatePassword({ email, password }: { email: UserDocument['email']; password: string }) {
+  const user = await User.findOne({ email });
+
+  if (!user) {
+    return false;
+  }
+
+  const isValid = await user.comparePassword(password);
+
+  if (!isValid) {
+    return false;
+  }
+
+  return omit(user.toJSON(), 'password');
+}
+
+// find a user
+export async function findUser(query: FilterQuery<UserDocument>) {
+  return User.findOne(query).lean();
 }
