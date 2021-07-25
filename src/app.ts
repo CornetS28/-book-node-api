db31ce0ac6a467f0540ea9df3180df3095a7a136
Samuel Cornet <corsam28@gmail.com>
2021-07-25 14:56:26 -0400

    feat: create mongoose db

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import config from 'config';
 import log from './logger';
+import connect from './db/connect';
 
 const port = config.get('port') as number;
 const host = config.get('host') as string;
@@ -11,5 +12,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.listen(port, host, () => {
-  log.info(`Server listenting at http://${host}:${port}`);
+  log.info(`Server listening at http://${host}:${port}`);
+
+  connect();
 });
