74e54880d983c6522f5565a2497e96672f52c914
Samuel Cornet <corsam28@gmail.com>
2021-07-25 15:30:13 -0400

    chore: add route for creating a user

--- src/routes.ts
+++ src/routes.ts
@@ -1,5 +1,10 @@
 import { Express, Request, Response } from 'express';
-
+import { createUserHandler } from './controller/user.controller';
+import { validateRequest } from './middleware';
+import { createUserSchema } from './schema/user.schema';
 export default function (app: Express) {
   app.get('/route-health-check', (req: Request, res: Response) => res.sendStatus(200));
+
+  // User Registration
+  app.post('/api/users', validateRequest(createUserSchema), createUserHandler);
 }
