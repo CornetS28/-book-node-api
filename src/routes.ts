263980e97c1f0555c07cc3423fae9237e172de4c
Samuel Cornet <corsam28@gmail.com>
2021-07-25 17:05:04 -0400

    feat: complte the sessions feature

--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,24 @@
 import { Express, Request, Response } from 'express';
 import { createUserHandler } from './controller/user.controller';
-import { validateRequest } from './middleware';
-import { createUserSchema } from './schema/user.schema';
+import { validateRequest, requiresUser } from './middleware';
+import {
+  createUserSessionHandler,
+  invalidateUserSessionHandler,
+  getUserSessionsHandler,
+} from './controller/session.controller';
+import { createUserSchema, createUserSessionSchema } from './schema/user.schema';
 export default function (app: Express) {
   app.get('/route-health-check', (req: Request, res: Response) => res.sendStatus(200));
 
-  // User Registration
+  // User registration
   app.post('/api/users', validateRequest(createUserSchema), createUserHandler);
+
+  // User login
+  app.post('/api/sessions', validateRequest(createUserSessionSchema), createUserSessionHandler);
+
+  // Get the user's sessions
+  app.get('/api/sessions', requiresUser, getUserSessionsHandler);
+
+  // Logout
+  app.delete('/api/sessions', requiresUser, invalidateUserSessionHandler);
 }
