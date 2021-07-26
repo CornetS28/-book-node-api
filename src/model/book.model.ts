25c395b95c1c74f6211a377fa31b4243f8983af8
Samuel Cornet <corsam28@gmail.com>
2021-07-25 20:14:03 -0400

    feat: implete the controller, model, and service for the books

---
+++ src/model/book.model.ts
@@ -0,0 +1,37 @@
+import mongoose from 'mongoose';
+import { nanoid } from 'nanoid';
+import { UserDocument } from './user.model';
+
+export interface BookDocument extends mongoose.Document {
+  user: UserDocument['_id'];
+  title: string;
+  description: string;
+  publisher: string;
+  publicationYear: number;
+  ISBN: string;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+const BookSchema = new mongoose.Schema(
+  {
+    bookId: {
+      type: String,
+      required: true,
+      unique: true,
+      default: () => nanoid(10),
+    },
+
+    user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+    title: { type: String, default: true },
+    description: { type: String, default: true },
+    publisher: { type: String, default: true },
+    publicationYear: { type: Number, default: true },
+    ISBN: { type: String, default: true, unique: true },
+  },
+  { timestamps: true }
+);
+
+const Book = mongoose.model<BookDocument>('Book', BookSchema);
+
+export default Book;
---
+++ src/schema/book.schema.ts
@@ -0,0 +1,32 @@
+import { object, string, number, ref } from 'yup';
+
+const payload = {
+  body: object({
+    title: string().required('Title is required').min(8, 'Title is too short - should be 8 chars minimum.'),
+    description: string()
+      .required('Description is required')
+      .min(120, 'Description is too short - should be 120 chars minimum.'),
+    publisher: string().required('Publisher is required').min(3, 'Publisher is too short - should be 3 chars minimum.'),
+    publicationYear: number().required('Publication year is required'),
+    ISBN: string().required('ISBN is required').min(13, 'ISBN should be 13 chars minimum.'),
+  }),
+};
+
+const params = {
+  params: object({
+    bookId: string().required('bookId is required'),
+  }),
+};
+
+export const createBookSchema = object({
+  ...payload,
+});
+
+export const updateBookSchema = object({
+  ...params,
+  ...payload,
+});
+
+export const deleteBookSchema = object({
+  ...params,
+});
